017383a1df68328e0514c2ef2b09c310f8bae8c9
Samu22203 <112439158+Samu22203@users.noreply.github.com>
2024-06-04 15:23:42 -0600

    Add files via upload

---
+++ Proyecto/Main/main.py
@@ -0,0 +1,17 @@
+""" 
+Título del proyecto: Analizador de Sentimientos de Archivo de Texto
+
+Descripción: Este proyecto tiene como objetivo desarrollar un programa en Python que permita analizar el sentimiento 
+(positivo, negativo o neutral) expresado en mensajes de texto almacenados en un archivo. 
+El programa utilizará técnicas de procesamiento de texto y análisis de sentimientos para extraer opiniones y 
+emociones de los mensajes, proporcionando información valiosa sobre la percepción del público hacia un tema o producto específico.
+
+
+"""
+
+
+
+def main():
+    
+    
+main()
